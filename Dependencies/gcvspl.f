8c2eb7f87a070b3b063517bd4dd91fedb2aadb81
Charles Le Losq <charles.lelosq@gmail.com>
2016-03-01 10:11:31 +1100

    Update of the licence/copyright

--- Dependencies/gcvspl.f
+++ Dependencies/gcvspl.f
@@ -1,12 +1,21 @@
 c
 c GCVSPL.FOR, 1986-05-12
 c (C) COPYRIGHT 1985, 1986: H.J. Woltring
-c See gcvspl.licence for the licence disclosure of this code.
-c ####
-c Note of Charles Le Losq:
+c This software is copyrighted, and may be  copied  for  exercise,
+c study  and  use  without authorization from the copyright owner(s), in
+c compliance with paragraph 16b of  the  Dutch  Copyright  Act  of  1912
+c ("Auteurswet  1912").  Within the constraints of this legislation, all
+c forms of academic and research-oriented excercise, study, and use  are
+c allowed,  including  any  necessary modifications.  Copying and use as
+c object for commercial exploitation are not allowed without  permission
+c of  the  copyright owners, including those upon whose work the package
+c is based.
+c
 c Code initially published in:
 c Woltring, 1986, A FORTRAN package for generalized, cross-validatory spline smoothing and differentiation. Adv. Eng. Softw. 8:104-113. 
-c Initial code available on www.netlib.org, other versions are available on https://isbweb.org/software/sigproc.html
+c ####
+c Note of Charles Le Losq: 
+c Code available on www.netlib.org, other versions are available on https://isbweb.org/software/sigproc.html
 c This is a Fortran 77 version downloaded on https://isbweb.org/software/sigproc.html
 c***********************************************************************
 c
