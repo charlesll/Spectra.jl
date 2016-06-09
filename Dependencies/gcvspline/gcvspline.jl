2f3b0d9346ce5b82dd1886ee923cc0e63ff4df31
charles.lelosq <charles.lelosq@gmail.com>
2016-06-09 08:37:05 +0000

    juliabox 2016-06-09 08:37:05.428242+00:00

--- Dependencies/gcvspline/gcvspline.jl
+++ Dependencies/gcvspline/gcvspline.jl
@@ -5,7 +5,7 @@ unshift!(PyVector(pyimport("sys")["path"]), "")
 
 function Gspline(xfit::Array{Float64},yfit::Array{Float64},esefit::Array{Float64},xtarget::Array{Float64},smoothing::Array{Float64};VAL::Array{Float64}= esefit.^2,splorder::Float64=2.,splmode::Float64=3.,NC::Float64 = size(yfit,1))
 	
-	c, wk, ier = gcvspline(xfit,yfit,smoothing.*ese,VAL,splmode, splorder,NC) # gcvspl with mode 3 and smooth factor
-	out::Array{Float64} = splderivative(x,xfit,c,splorder; L = 1,IDER = 0) 
+    c, wk, ier = gcvspline.gcvspline(xfit,yfit,smoothing.*ese,VAL,splmode, splorder,NC) # gcvspl with mode 3 and smooth factor
+    out::Array{Float64} = gcvspline.splderivative(x,xfit,c,splorder; L = 1,IDER = 0) 
 	
 end
