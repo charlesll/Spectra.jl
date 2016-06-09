c7b8a5d5596c1ed8955bf677751da8de496a1f99
charles.lelosq <charles.lelosq@gmail.com>
2016-06-09 23:09:06 +0000

    PyCall of gcvspline working!

--- Dependencies/gcvspline/gcvspline.jl
+++ Dependencies/gcvspline/gcvspline.jl
@@ -3,9 +3,7 @@ unshift!(PyVector(pyimport("sys")["path"]), "")
 
 @pyimport gcvspline
 
-function Gspline(xfit::Array{Float64},yfit::Array{Float64},esefit::Array{Float64},xtarget::Array{Float64},smoothing::Array{Float64};VAL::Array{Float64}= esefit.^2,splorder::Float64=2.,splmode::Float64=3.,NC::Float64 = size(yfit,1))
-	
-    c, wk, ier = gcvspline.gcvspline(xfit,yfit,smoothing.*ese,VAL,splmode, splorder,NC) # gcvspl with mode 3 and smooth factor
-    out::Array{Float64} = gcvspline.splderivative(x,xfit,c,splorder; L = 1,IDER = 0) 
-	
+function Gspline(xfit::Array{Float64},yfit::Array{Float64},esefit::Array{Float64},xtarget::Array{Float64},smoothing::Float64;VAL = esefit.^2,splor=2,splm=3,nc = size(yfit,1))
+    c, wk, ier = gcvspline.gcvspline(xfit,yfit,smoothing.*ese,VAL,splmode=splm, splorder=splor,NC=nc) # gcvspl with mode 3 and smooth factor
+    out = gcvspline.splderivative(x,xfit,c,splorder=splor, L = 1,IDER = 0) 
 end
