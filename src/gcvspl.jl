cfbaf4b47f7e2de0e7028cd1a5a342b0dcaedd7a
Charles Le Losq <charles.lelosq@gmail.com>
2016-06-20 10:34:01 +1000

    get automatically the good paths for gcvspline

--- src/gcvspl.jl
+++ src/gcvspl.jl
@@ -86,7 +86,7 @@ function gcvspl(x::Array{Float64,1},y::Array{Float64,1},ese::Array{Float64,1},Sm
 	c = ones(N,NC)
 	WK = ones(6*(N*SplineOrder+1)+N,1)
 	IER=Int32[1]
-	ccall( (:gcvspl_, "/Users/charles/.julia/v0.4/Spectra/Dependencies/gcvspline/libgcvspl.so"), Void, (Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Float64},Ref{Float64},Ptr{Cint},Ref{Float64},Ref{Cint}),x,y,&N,WX,WY,&SplineOrder,&N,&K,&SplineMode,VAL,c,&NC,WK,IER)
+	ccall( (:gcvspl_, abspath("../Dependencies/gcvspline/libgcvspl.so"), Void, (Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Float64},Ref{Float64},Ptr{Cint},Ref{Float64},Ref{Cint}),x,y,&N,WX,WY,&SplineOrder,&N,&K,&SplineMode,VAL,c,&NC,WK,IER)
 	return c, WK, IER
 end
 
@@ -120,7 +120,7 @@ function splderivative(xfull::Array{Float64},xparse::Array{Float64},cparse::Arra
     
     # we loop other xfull to create the output values
 	for i =1:size(y_calc,1)
-	    y_calc[i] = ccall( (:splder_, "/Users/charles/.julia/v0.4/Spectra/Dependencies/gcvspline/libgcvspl.so"), Float64, (Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Float64}),&IDER, &SplineOrder, &N, &xfull[i], xparse, cparse, &L, q)
+	    y_calc[i] = ccall( (:splder_, abspath("../Dependencies/gcvspline/libgcvspl.so")), Float64, (Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Float64},Ptr{Float64},Ptr{Float64},Ptr{Cint},Ptr{Float64}),&IDER, &SplineOrder, &N, &xfull[i], xparse, cparse, &L, q)
 	end
 	return y_calc
 end
