6210840f1eba8c85a3a502be15c65bff3519f374
Charles Le Losq <charles.lelosq@gmail.com>
2017-05-16 17:05:31 +1000

    doc test 5:05pm

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,6 @@
 using Documenter, Spectra
 
-makedocs(
-    modules = [Spectra],
-    format = :html,
-    sitename = "Spectra.jl",
-    pages = Any[
-        "Home" => "index.md",
-		"Installation" => "Installation.md",
-		"Pre-Processing" => "PreProcessing.md",
-		"General Functions" => "GeneralFunctions.md",
-		"Rameau" => "Rameau.md",
-		"Machine Learning Regression" => "MLregressor.md",
-		"Peak Fitting" => "PeakFitting.md",
-		"Tutorial" => "Tutorial.md",
-		"Tips" => "Tips.md",
-		"To do" => "ToDo.md",
-		"References" => "References.md"
-    ],
-    doctest = false
-)
+makedocs()
 
 deploydocs(
     repo = "github.com/charlesll/Spectra.jl.git", 
