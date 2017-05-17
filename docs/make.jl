1c665e04c66cfa5685e404179050323560c8d5ca
Charles Le Losq <charles.lelosq@gmail.com>
2017-05-17 13:07:47 +1000

    20170517 #6 trying to add the material theme

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, Spectra
 makedocs()
 
 deploydocs(
+    deps = Deps.pip("mkdocs", "mkdocs-material"),
     repo = "github.com/charlesll/Spectra.jl.git", 
 	julia  = "0.5",
 	osname = "linux",
