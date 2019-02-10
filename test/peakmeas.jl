036f1d154d7ea8edf1ce4c6bd77618e2590c54d7
Charles Le Losq <charles.lelosq@gmail.com>
2019-02-11 10:07:09 +1100

    smooth in peakmeas

--- test/peakmeas.jl
+++ test/peakmeas.jl
@@ -17,7 +17,7 @@ y_noise = y+randn((size(y,1),1))
 
 # we measured the width and frequency with the peakhw function
 int_mea1, freq_meas1, hwhm_meas1, centroid_mea1 = peakmeas(x,y,smoothing="no")
-int_mea2, freq_meas2, hwhm_meas2, centroid_mea2 = peakmeas(x,y_noise,M=9,N=5) # with applying a Savitsky Golay filter
+int_mea2, freq_meas2, hwhm_meas2, centroid_mea2 = peakmeas(x,y_noise,window_length=9,polyorder=5) # with applying a Savitsky Golay filter
 
 # test of the perfect version
 @test isapprox(int_th,int_mea1,atol=1e-5)
