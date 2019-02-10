b56f4f1983405ff9232c02dbc993f03bb3851b91
Charles Le Losq <charles.lelosq@gmail.com>
2019-02-11 10:28:47 +1100

    tlcorrection test OK

--- test/tlcorrection.jl
+++ test/tlcorrection.jl
@@ -12,7 +12,7 @@ nu = 100.0.*x_for_long # cm-1 to m-1
 T = 23.0+273.15 # the temperature in K
 
 x_long,long_res,eselong = tlcorrection([x_for_long y_for_long],23.0,514.532) # using the function
-t0 = nu0.^3.*nu./((nu0.-nu).^4)
+t0 = nu0.^3.0 .*nu./((nu0.-nu).^4)
 t1= 1 .- exp.(-h_plank.*c_light.*nu./(k_bolt.*T)) # c in m/s  : t1 dimensionless
 long_calc= y_for_long.*t0.*t1 # pour les y
 long_calc = long_calc./trapz(x_for_long,long_calc) # area normalisation
